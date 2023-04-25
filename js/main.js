39be2e1faa6b128bd5d2421da05e0f07730eb4f8
OvsyankinVI <ovsyankin717@gmail.com>
2023-04-25 17:55:35 +0500

    230425_Add mail

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-    var modal = document.querySelectorAll('.modal');
-    var instances_modal = M.Modal.init(modal);
-  }); 
-  function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-       change.target.classList.add('element-show');
-      }
-    });
-  }
-  function onHeader(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-       change.target.classList.add('header-bg');
-      }
-    });
-  }
-  let options = {
-    threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let header = new IntersectionObserver(onHeader, options);
-  let elements_1 = document.querySelectorAll('.item-1');
-  let elements_2 = document.querySelectorAll('.item-2');
-  let elements_3 = document.querySelectorAll('.item-3');
-  let header_bg = document.querySelectorAll('.header__flex');
-  
-  for (let elm of elements_1) {
-    observer.observe(elm);
-  }
-  for (let elm of elements_2) {
-    observer.observe(elm);
-  }
-  for (let elm of elements_3) {
-    observer.observe(elm);
-  }
-  for (let elm of header_bg) {
-    header.observe(elm);
-  }
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+  var modal = document.querySelectorAll('.modal');
+  var instances_modal = M.Modal.init(modal);
+});
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+function onHeader(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('header-bg');
+    }
+  });
+}
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let header = new IntersectionObserver(onHeader, options);
+let elements_1 = document.querySelectorAll('.item-1');
+let elements_2 = document.querySelectorAll('.item-2');
+let elements_3 = document.querySelectorAll('.item-3');
+let header_bg = document.querySelectorAll('.header__flex');
+
+for (let elm of elements_1) {
+  observer.observe(elm);
+}
+for (let elm of elements_2) {
+  observer.observe(elm);
+}
+for (let elm of elements_3) {
+  observer.observe(elm);
+}
+for (let elm of header_bg) {
+  header.observe(elm);
+}
